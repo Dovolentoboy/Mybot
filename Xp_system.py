7fb3b88d88c118b75059fe063e72a105150ae07c
Nikita <121382716+Dovolentoboy@users.noreply.github.com>
2023-05-21 10:54:56 +0300

    Добавление комментария

--- Xp_system.py
+++ Xp_system.py
@@ -11,7 +11,7 @@ class Xp_system(commands.Cog):
 
     def __init__(self,bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_message(self,message):
         xp = cursor.execute("SELECT xp FROM users WHERE member_id = ?",(message.author.id,)).fetchone()[0]
@@ -40,6 +40,38 @@ class Xp_system(commands.Cog):
                     role_id = lvl_roles[lvl + 1]
                     role = message.guild.get_role(role_id)
                     await message.author.add_roles(role)
+
+
+
+"""
+
+Для непонимающих. В этом ивенте бот читает сообщения и исходя из них делает определенные дейсвтия. Узнать вы можете об этом в документации или же через вопросы к нейросети
+Хочу также отметить,что вы можете использовать формулу для уровня какую хотите. Например мой товарищ предложил сделать систему опыта через длину сообщений
+Также можете сделать свою систему лвлов. Здесь предложен мой вараинт(взял я его у Fsoky)
+Если что-то будет не работать обращайтесь в Dovolen toboy#6686
+
+Удачного кодинга , поменьше ошибок и багов. Верю в вас)
+
+P.S Ниже код для длины сообщений
+
+@commands.Cog.listener()
+    async def on_message(self,message):
+        xp_count = len(message.content) * 0.3
+        xp = cursor.execute("SELECT xp FROM users WHERE member_id = ?",(message.author.id,)).fetchone()[0]
+        lvl = cursor.execute("SELECT lvl FROM users WHERE member_id = ?",(message.author.id,)).fetchone()[0]
+        if message.author == self.bot:
+            return
+        if message.interaction is not None:
+            return
+        else:
+             cursor.execute(f"UPDATE users SET xp = xp + {xp_count} WHERE member_id = ?",(message.author.id,))
+             db.commit()
+             
+
+Остальное можете оставить неизменным, т.е тупо взять всё с кода выше)
+"""
+
+
 
 def setup(bot):
     bot.add_cog(Xp_system(bot))
